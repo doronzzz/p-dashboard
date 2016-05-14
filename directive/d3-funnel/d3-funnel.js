7a89de3a4743a1847efabf0713b4580808ca87b8
doronzzz <gourdoron@gmail.com>
2016-05-14 23:52:07 +0300

    styling + funnel directive changes

--- directive/d3-funnel/d3-funnel.js
+++ directive/d3-funnel/d3-funnel.js
@@ -1,18 +1,39 @@
 angular.module('pontis').directive('d3Funnel', function() {
     return {
         restrict: 'E',
-        replace: true,
+        replace: false,
         scope: {
-
+            height: "=height",
+            width: "=width",
         },
         templateUrl: 'directive/d3-funnel/d3-funnel.html',
         link: function(scope, element, attrs, fn) {
         	 var data = [
-		        ['Total Base',     5000 ,"#86ADE8"],
-		        ['Available',    2500 ,"#64BBE8"],
-		        ['Assigned', 200 ,"#66C2CD"]
+
+                
+                    ['Total Base',     5000 ,"#86ADE8"],
+                    ['Available',    2500 ,"#64BBE8"],
+                    ['Assigned', 200 ,"#66C2CD"]
+                
+
 		    ];
-		    var options = {};
+		    var options = {
+                chart:{
+                    height:scope.height,
+                    width:scope.width,
+                    block:{
+                        highlight : true
+                    },
+                    label:{
+                        format:'{l}:- ${f}'
+                    },
+                    bottomPinch:0,
+                    curve:{
+                        enabled:true,
+                        height:15
+                    }
+                }
+            };
 		    // var selector = $(element).find('.myFunnel');
 		    var chart = new D3Funnel(element[0]);
 		    chart.draw(data, options);
